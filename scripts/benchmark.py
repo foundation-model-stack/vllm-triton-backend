49b20476a485938077d8f070f9e215f2854aaf9b
Burkhard Ringlein <ngl@zurich.ibm.com>
2025-03-10 08:50:38 -0400

    integrating fused into decode benchmarak

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -75,12 +75,12 @@ class BenchmarkMode(Enum):
 DTYPES = [torch.float16]
 SEEDS = [0]
 
-# BATCH_SIZES = [1, 2, 4, 8, 16, 32, 64, 128]
+BATCH_SIZES = [1, 2, 4, 8, 16, 32, 64, 128]
 # BATCH_SIZES = [128]
 # BATCH_SIZES = [64]
 # BATCH_SIZES = [2]
 # BATCH_SIZES = [1, 2, 4, 8, 16, 32, 64, 128, 256]
-BATCH_SIZES = [1, 2, 3, 4, 5, 7, 8, 12, 16, 32, 64, 128]
+# BATCH_SIZES = [1, 2, 3, 4, 5, 7, 8, 12, 16, 32, 64, 128]
 
 # order:  num_query_heads, num_kv_heads
 # NUM_HEADS = [(32, 32), (32, 8)]
@@ -375,6 +375,8 @@ def test_decode_attention(
             from callers import FlashAttnDecodeCaller as Caller
         elif implementation == Implementation.FLASHINFER:
             from callers import FlashInferCaller as Caller
+        elif implementation == Implementation.TRITON_FUSED:
+            from callers import FusedTritonDecodeOnlyCaller as Caller
 
         if Caller.requires_allocated_output:
             output = torch.empty_like(query)
--- scripts/callers/fused_triton.py
+++ scripts/callers/fused_triton.py
@@ -18,7 +18,68 @@
 
 import torch
 from ibm_triton_lib.kernels import fused_chunked_prefill_paged_decode_25d
-from .base import PrefixPrefillCaller
+from .base import PrefixPrefillCaller, DecodeCaller
+
+
+class FusedTritonDecodeOnlyCaller(DecodeCaller):
+    @staticmethod
+    def make_call_func(
+        output,
+        query,
+        key_cache,
+        value_cache,
+        num_seqs,
+        seq_lens,
+        max_seq_len,
+        scale,
+        block_tables,
+        alibi_slopes,
+        kv_cache_dtype,
+    ):
+        """
+        query: shape = [num_tokens, num_heads, head_size]
+        key: shape = [num_tokens, num_kv_heads, head_size]
+        value: shape = [num_tokens, num_kv_heads, head_size]
+        kv_cache = [2, num_blocks, block_size, num_kv_heads, head_size]
+        """
+        
+        query_lens = [1] * num_seqs
+        b_query_lens = torch.tensor(query_lens, dtype=torch.int)
+        b_start_loc = torch.cumsum(
+            torch.tensor([0] + query_lens, dtype=torch.int), dim=0, dtype=torch.int
+        )
+
+        max_query_len = max(query_lens)
+        # print(query.shape)
+        # print(key_cache.shape)
+        # print(value_cache.shape)
+        k_scale = v_scale = torch.tensor(1.0, dtype=torch.float32, device=query.device)
+
+        def call_and_process_output():
+            return fused_chunked_prefill_paged_decode_25d(
+                query=query,
+                key=key_cache, # would break, just here for benchmarking
+                value=value_cache, # would break, just here for benchmarking
+                output=output,
+                kv_cache_dtype=kv_cache_dtype,  # TODO
+                key_cache=key_cache,
+                value_cache=value_cache,
+                block_table=block_tables,
+                query_start_loc=b_start_loc,
+                seq_lens=seq_lens,
+                max_query_len=max_query_len,
+                k_scale=k_scale,
+                v_scale=v_scale,
+                alibi_slopes=None,  # TODO
+                sliding_window=None,  # TODO
+                sm_scale=1.0, # would break, just here for benchmarking
+            )
+
+        return call_and_process_output
+
+    @staticmethod
+    def requires_allocated_output() -> bool:
+        return True
 
 
 class FusedTritonChunkedPrefixPrefill25dCaller(PrefixPrefillCaller):
