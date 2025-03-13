96f4f752d477897e5e1499a2fe3365d07bb1f741
Jan van Lunteren <jvl@zurich.ibm.com>
2025-03-13 06:12:48 -0400

    various updates
    Signed-off-by: Jan van Lunteren <jvl@zurich.ibm.com>

--- scripts/callers/triton_3d.py
+++ scripts/callers/triton_3d.py
@@ -17,8 +17,8 @@
 
 
 import torch
-from ibm_triton_lib.kernels import paged_attention_3d, prefill_flash_attention
-from .base import DecodeCaller, PrefillCaller
+from ibm_triton_lib.kernels import paged_attention_3d
+from .base import DecodeCaller
 
 
 class Triton3dAttentionDecodeCaller(DecodeCaller):
@@ -34,7 +34,7 @@ class Triton3dAttentionDecodeCaller(DecodeCaller):
         scale,
         block_tables,
         alibi_slopes,
-        kv_cache_dtype,  # unused
+        kv_cache_dtype,
     ):
         num_query_heads = query.shape[1]
         num_kv_heads = key_cache.shape[1]
@@ -43,12 +43,18 @@ class Triton3dAttentionDecodeCaller(DecodeCaller):
         max_num_blocks_per_seq = block_tables.shape[1]
         head_size = key_cache.shape[2]
 
+        # Using default kv_scale
+        k_scale = v_scale = torch.ones(1, device=query.device)
+
         call_func_under_test = lambda: paged_attention_3d(
             output,
             query,
             key_cache,
             value_cache,
             scale,
+            k_scale,
+            v_scale,
+            kv_cache_dtype,
             block_tables,
             seq_lens,
             alibi_slopes,
@@ -60,48 +66,3 @@ class Triton3dAttentionDecodeCaller(DecodeCaller):
         )
 
         return call_func_under_test
-
-
-class Triton3dAttentionPrefillCaller(PrefillCaller):
-    @staticmethod
-    def make_call_func(
-        output,
-        query,
-        key_cache,
-        value_cache,
-        cu_seqlens_q,
-        cu_seqlens_k,
-        max_seqlen_q,
-        max_seqlen_k,
-        softmax_scale,
-        causal,
-        # kv_cache_dtype,  # unused
-    ):
-        # with varlen
-        # q: (total_q, nheads, headdim), where total_q = total number of query tokens in the batch.
-        # k: (total_k, nheads_k, headdim), where total_k = total number of key tokens in the batch.
-        # v: (total_k, nheads_k, headdim), where total_k = total number of key tokens in the batch.
-        # cu_seqlens_q: (batch_size + 1,), dtype torch.int32. The cumulative sequence lengths
-        #    of the sequences in the batch, used to index into q.
-        # cu_seqlens_k: (batch_size + 1,), dtype torch.int32. The cumulative sequence lengths
-        #    of the sequences in the batch, used to index into kv.
-        # max_seqlen_q: int. Maximum query sequence length in the batch.
-        # max_seqlen_k: int. Maximum key sequence length in the batch.
-        # out: (total, nheads, headdim).
-
-        def call_and_process_output():
-            prefill_flash_attention(
-                q=query,
-                k=key_cache,
-                v=value_cache,
-                causal=causal,
-                sm_scale=softmax_scale,
-                max_seqlen_q=max_seqlen_q,
-                max_seqlen_k=max_seqlen_k,
-                cu_seqlens_q=cu_seqlens_q,
-                cu_seqlens_k=cu_seqlens_k,
-                in_place_output=output,
-                do_not_return_softmax_encodings=True,
-            )
-
-        return call_and_process_output
