a02655fb315ef562dd9f212a51ef060acb070e0c
Burkhard Ringlein <ngl@zurich.ibm.com>
2025-06-27 14:26:03 +0000

    updating label detection in ibm_triton_lib
    Signed-off-by: Burkhard Ringlein <ngl@zurich.ibm.com>

--- ibm-triton-lib/ibm_triton_lib/utils/triton_utils.py
+++ ibm-triton-lib/ibm_triton_lib/utils/triton_utils.py
@@ -63,45 +63,6 @@ rocm_version = None
 flag_print_debug = False
 
 
-def _get_rocm_version():
-    """
-    Get ROCM runtime/driver version (i.e. which rocm linker is used).
-    This version is often different from the rocm version pytorch uses internally.
-    """
-    global rocm_version
-    if rocm_version is not None:
-        return rocm_version
-    if "_TRITON_DEJAVU_DETERMINED_ROCM_VERSION" in os.environ:
-        rocm_version = os.environ["_TRITON_DEJAVU_DETERMINED_ROCM_VERSION"]
-        return rocm_version
-    try:
-        import subprocess
-        import re
-
-        rocm_ldd_path = triton.backends.backends["amd"].compiler.path_to_rocm_lld()
-        rocm_dir = os.path.dirname(rocm_ldd_path)
-        amdgpu_arch_path = os.path.abspath(os.path.join(rocm_dir, "amdgpu-arch"))
-
-        result = subprocess.check_output(
-            [amdgpu_arch_path, "--version"],
-            stderr=subprocess.STDOUT,
-        )
-        version = re.search(
-            r".*roc-(\d+\.\d+.\d+).*", result.decode("utf-8"), flags=re.MULTILINE
-        )
-        rocm_version = version.group(1)
-    except Exception as e:
-        if flag_print_debug:
-            print(f"[triton-dejavu] determining rocm version failed with: {e}")
-        cuda_version = os.environ.get("CONTAINER_ROCM_VERSION", "unknown")
-        if cuda_version == "unknown":
-            raise Exception(
-                "Can't determine cuda version and also CONTAINER_ROCM_VERSION is not set"
-            )
-    os.environ["_TRITON_DEJAVU_DETERMINED_ROCM_VERSION"] = rocm_version
-    return rocm_version
-
-
 def _get_cuda_version():
     """
     Get CUDA runtime/driver version (i.e. which ptxas is used).
@@ -133,14 +94,52 @@ def _get_cuda_version():
         cuda_version = version.group(1)
     except Exception as e:
         if flag_print_debug:
-            print(f"[triton-dejavu] determining cuda version failed with: {e}")
-        cuda_version = os.environ.get("CONTAINER_CUDA_VERSION", "unknown")
-        if cuda_version == "unknown":
-            raise Exception(
-                "Can't determine cuda version and also CONTAINER_CUDA_VERSION is not set"
+            print(
+                f"[triton-dejavu] determining cuda version failed with: {e}\n"
+                f"using torch.version.cuda as fallback"
             )
+        cuda_version = f"torch_{torch.version.cuda}"
     os.environ["_TRITON_DEJAVU_DETERMINED_CUDA_VERSION"] = cuda_version
     return cuda_version
+
+
+def _get_rocm_version():
+    """
+    Get ROCM runtime/driver version (i.e. which rocm linker is used).
+    This version is often different from the rocm version pytorch uses internally.
+    """
+    global rocm_version
+    if rocm_version is not None:
+        return rocm_version
+    if "_TRITON_DEJAVU_DETERMINED_ROCM_VERSION" in os.environ:
+        rocm_version = os.environ["_TRITON_DEJAVU_DETERMINED_ROCM_VERSION"]
+        return rocm_version
+    try:
+        import subprocess
+        import re
+
+        rocm_ldd_path = triton.backends.backends["amd"].compiler.path_to_rocm_lld()
+        rocm_dir = os.path.dirname(rocm_ldd_path)
+        amdgpu_arch_path = os.path.abspath(os.path.join(rocm_dir, "amdgpu-arch"))
+
+        result = subprocess.check_output(
+            [amdgpu_arch_path, "--version"],
+            stderr=subprocess.STDOUT,
+        )
+        version = re.search(
+            r".*roc-(\d+\.\d+.\d+).*", result.decode("utf-8"), flags=re.MULTILINE
+        )
+        rocm_version = version.group(1)
+    except Exception as e:
+        if flag_print_debug:
+            print(
+                f"[triton-dejavu] determining rocm version failed with: {e}\n"
+                f"using torch.version.hip as fallback"
+            )
+        rocm_version = f"torch_{torch.version.hip}"
+    os.environ["_TRITON_DEJAVU_DETERMINED_ROCM_VERSION"] = rocm_version
+    return rocm_version
+
 
 
 def get_runtime_label():
