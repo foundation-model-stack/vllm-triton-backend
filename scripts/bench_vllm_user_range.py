80e9ea09524cc2b4c31ae6432a1bc245a640ada4
Burkhard Ringlein <ngl@zurich.ibm.com>
2025-07-29 12:08:15 -0400

    updating serving range script
    Signed-off-by: Burkhard Ringlein <ngl@zurich.ibm.com>

--- scripts/bench_vllm_user_range.py
+++ scripts/bench_vllm_user_range.py
@@ -40,14 +40,17 @@ def create_dir_if_not_exist(path, mode=0o777):
         except PermissionError as e:
             print(f"can't set permission of directory {path}: {e}")
 
+if len(sys.argv) < 4:
+    print(f"Usage: {sys.argv[0]} <model_path> <testcase_name> <result_path>")
+    exit(-1)
 
 num_users_to_test = [1, 2, 4, 8, 16, 32, 64, 128]
 gpu_name = torch.cuda.get_device_name().replace(" ", "_").replace("/", "_")
 
 # model = "/model/llama3.1-8b/instruct/"
 model = sys.argv[1]
-model_path = f"/models/{model}/"
 testcase_name = sys.argv[2]
+result_path = os.path.abspath(sys.argv[3])
 
 # max_rounds = 128
 max_rounds = 64
@@ -55,14 +58,22 @@ max_num_prompts = 1000
 
 timestamp_f = datetime.now().strftime("%Y-%m-%d_%H%M")
 
-# result_dir = f"/results/{model.replace('/','-')}/{gpu_name}/{testcase_name}"
-result_dir = (
-    f"/results/{model.replace('/','-')}/{gpu_name}/{testcase_name}/exp_{timestamp_f}/"
-)
+# result_dir = (
+#     f"/results/{model.replace('/','-')}/{gpu_name}/{testcase_name}/exp_{timestamp_f}/"
+# )
+result_dir = f"{result_path}/{model.replace('/','-')}/{gpu_name}/{testcase_name}/exp_{timestamp_f}/"
+
+bench_script = "/workspace/benchmarks/benchmark_serving.py"
+if not os.path.isfile(bench_script):
+    bench_script = "./vllm-triton-backend/vllm/benchmarks/benchmark_serving.py"
+    if not os.path.isfile(bench_script):
+        print(f"can't find benchmark script benchmark_serving.py")
+        exit(-1)
 
 # os.system(f"mkdir -p {result_dir}")
 create_dir_if_not_exist_recursive(result_dir)
 
+start_time = datetime.now()
 for max_concurrency in num_users_to_test:
     num_prompts = (
         max_num_prompts
@@ -70,12 +81,13 @@ for max_concurrency in num_users_to_test:
         else int(max_rounds * max_concurrency)
     )
     cmd = (
-        f"VLLM_USE_V1=1 python /workspace/benchmarks/benchmark_serving.py "
-        f"--model {model_path} "
+        f"VLLM_USE_V1=1 python {bench_script} "
+        f"--model {model} "
         f"--dataset-name sharegpt --dataset-path ShareGPT_V3_unfiltered_cleaned_split.json "
         f"--save-result --result-dir {result_dir} --max-concurrency {max_concurrency} "
         f"--percentile-metrics ttft,tpot,itl,e2el --metric-percentiles 20,50,80,99 "
         f"--num-prompts {num_prompts} "
+        f"--port 8803"
     )
     print(cmd)
     rv = os.system(cmd)
@@ -83,5 +95,7 @@ for max_concurrency in num_users_to_test:
         print(f"benchmark command returned {rv}, stopping...")
         break
 
+end_time = datetime.now()
 print(f"results stored in: {result_dir}")
 os.system(f"ls -alh {result_dir}")
+print(f"Benchmark time: {end_time-start_time}")
